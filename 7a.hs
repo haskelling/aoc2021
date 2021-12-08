59600fb1ef54e2b6d2bddb69eecba27a4d35969c
Haskell Coder <haskelling@pdr.cx>
2021-12-08 16:48:50 +0100

    Day 7

---
+++ 7a.hs
@@ -0,0 +1,13 @@
+import AOC
+
+main = interact' $ f . map (read :: String -> Int) . splitOn ","
+
+f xs = minimum' (minimum xs) (maximum xs) cost
+  where
+    minimum' low high f | high - low < 3 = minimum $ map f [low .. high]
+    minimum' low high f = let x = (low + high) `quot` 2
+                          in  if f (x + 1) > f x
+                                then minimum' low x f
+                                else minimum' x high f
+
+    cost p = sum $ map (abs . (p-)) xs
